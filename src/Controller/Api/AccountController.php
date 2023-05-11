069a859cb1b004531a549343cd55e5cf52c825e6
Sergey <sergey.lapin@silverbird.com>
2023-05-11 17:47:21 +0000

    Nelmio autodoc

--- src/Controller/Api/AccountController.php
+++ src/Controller/Api/AccountController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use OpenApi\Annotations as OA;
-
+use Nelmio\ApiDocBundle\Annotation\Model;
 use App\Entity\Person;
 
 /**
@@ -19,9 +19,18 @@ class AccountController extends AbstractController
     * @Route("/{personId}", methods={"GET"})
     * @ParamConverter("person", options={"mapping": {"personId": "id"}})
     *
+    * @OA\Tag(name="Accounts")
+    * @OA\Response(
+    *     response=200,
+    *     description="Deposit accounts list",
+    *     @OA\JsonContent(
+    *        type="object",
+    *        ref=@Model(type=GetListOfAccountsByPersonDto::class),
+    *     )
+    * )
     * @param Person $person
-     * @return Response
-     */
+    * @return Response
+    */
     public function viewAccountsAction(Person $person) {
         $response = new GetListOfAccountsByPersonDto();
         $response->setListOfAccounts($person->getAccounts()->toArray());
